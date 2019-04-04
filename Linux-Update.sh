67648d40c917042f8ccb7d2cb7febc0694293c99
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-04 01:02:00 -0400

    Create Linux-Update.sh

---
+++ Linux-Update.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Sorry, you need to run this as root"
+    exit
+fi
+
+
+if [ -e /etc/centos-release ]; then
+    DISTRO="CentOS"
+elif [ -e /etc/debian_version ]; then
+    DISTRO=$( lsb_release -is )
+else
+    echo "Your distribution is not supported (yet)"
+    exit
+fi
+
+if [ "$( systemd-detect-virt )" == "openvz" ]; then
+    echo "OpenVZ virtualization is not supported"
+    exit
+fi
+
+    if [ "$DISTRO" == "Ubuntu" ]; then
+        apt-get update
+        apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt-get install build-essential haveged linux-headers-$(uname -r) curl cron screen -y
+        apt-get autoremove -y
+        apt-get clean -y
+        
+    elif [ "$DISTRO" == "Debian" ]; then
+        apt-get update
+        apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt-get install build-essential haveged linux-headers-$(uname -r) curl cron screen -y
+        apt-get autoremove -y
+        apt-get clean -y
+        
+    elif [ "$DISTRO" == "CentOS" ]; then
+        yum update -y
+        yum install epel-release -y
+    fi
