6840417be600deed62206d3c1a20d26edd45cb40
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-04 01:38:12 -0400

    Create Install-Go-Lang.sh

---
+++ Install-Go-Lang.sh
@@ -0,0 +1,10 @@
+cd $HOME
+dpkg --print-architecture
+FileName='REPLACE_WITH_FILE_NAME'
+wget https://dl.google.com/go/$FileName
+sudo tar -C /usr/local -xvf $FileName
+cat >> ~/.bashrc << 'EOF'
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+EOF
+source ~/.bashrc
