9ac652e62dba26ca534564288af75695cfa40296
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-16 19:48:28 -0400

    Update Tor-Middle-Relay.sh

--- Tor-Middle-Relay.sh
+++ Tor-Middle-Relay.sh
@@ -35,49 +35,6 @@ fi
         apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
         apt-get autoremove -y
         apt-get clean -y
-        echo 'deb http://deb.torproject.org/torproject.org jessie main' >> /etc/apt/sources.list
-        apt-get update
-        apt-get upgrade -y
-        curl https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --import
-        gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
-        apt-get update
-        apt-get upgrade
-        apt-get install tor deb.torproject.org-keyring -y 
-        apt-get install tor -y 
-  echo 'SocksPort 0
-  Log notice file /var/log/tor/notices.log
-  RunAsDaemon 1
-  DataDirectory /var/lib/tor
-  ControlPort 9051
-  CookieAuthentication 1
-  ORPort 443
-  DirPort 80
-  ExitPolicy reject *:*
-  Nickname TypeYourNicknameHere
-  ContactInfo TypeYourEmailHere
-  DisableDebuggerAttachment 0' >> /etc/tor/torrc
-        sudo /etc/init.d/tor restart
-        sudo apt-get install tor-arm
-        sudo -u debian-tor arm
-  echo '##  Allows all loopback (lo0) traffic and drop all traffic to 127/8 that doesn't use lo0
--A INPUT -i lo -j ACCEPT
-      
-## allow incoming SSH      
--A INPUT -p tcp --dport 22 -j ACCEPT
-## allow Tor ORPort, DirPort        
--A INPUT -p tcp --dport 433 -j ACCEPT
--A INPUT -p tcp --dport 80 -j ACCEPT
-
-## ratelimit ICMP echo, allow all others
--A INPUT -p icmp --icmp-type echo-request -m limit --limit 2/s -j ACCEPT
--A INPUT -p icmp --icmp-type echo-request -j DROP
--A INPUT -p icmp -j ACCEPT
-
-## to log denied packets uncomment this line (I uncommented it for you).
--A INPUT -m limit --limit 5/min -j LOG --log-prefix "iptables denied: " --log-level 7
--A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
--A INPUT -m state --state INVALID -j DROP' >> /etc/iptables/rules.v4
-        
     elif [ "$DISTRO" == "CentOS" ]; then
         yum update -y
         yum install epel-release haveged kernel-devel -y
