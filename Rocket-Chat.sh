c91100ad75d656a4269ad4f0e19ebcfd24d9dec6
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-18 19:39:19 -0400

    Update Rocket-Chat.sh

--- Rocket-Chat.sh
+++ Rocket-Chat.sh
@@ -55,4 +55,25 @@ PORT=3000' >> /lib/systemd/system/rocketchat.service
         yum update -y
         yum install epel-release haveged kernel-devel -y
         yum groupinstall 'Development Tools' -y
+        echo -e "[mongodb-org-3.6]\nname=MongoDB Repository\nbaseurl=https://repo.mongodb.org/yum/redhat/7/mongodb-org/3.6/x86_64/\ngpgcheck=1\nenabled=1\ngpgkey=https://www.mongodb.org/static/pgp/server-3.6.asc" | sudo tee /etc/yum.repos.d/mongodb-org-3.6.repo
+        sudo yum install -y curl && curl -sL https://rpm.nodesource.com/setup_8.x | sudo bash -
+        sudo yum install -y gcc-c++ make mongodb-org nodejs
+        sudo yum install -y epel-release && sudo yum install -y GraphicsMagick
+        sudo npm install -g inherits n && sudo n 8.11.3
+        curl -L https://releases.rocket.chat/latest/download -o /tmp/rocket.chat.tgz
+        tar -xzf /tmp/rocket.chat.tgz -C /tmp
+        cd /tmp/bundle/programs/server && npm install
+        sudo mv /tmp/bundle /opt/Rocket.Chat
+        sudo useradd -M rocketchat && sudo usermod -L rocketchat
+        sudo chown -R rocketchat:rocketchat /opt/Rocket.Chat
+        echo -e "[Unit]\nDescription=The Rocket.Chat server\nAfter=network.target remote-fs.target nss-lookup.target nginx.target mongod.target\n[Service]\nExecStart=/usr/local/bin/node /opt/Rocket.Chat/main.js\nStandardOutput=syslog\nStandardError=syslog\nSyslogIdentifier=rocketchat\nUser=rocketchat\nEnvironment=LD_PRELOAD=/opt/Rocket.Chat/programs/server/npm/node_modules/sharp/vendor/lib/libz.so NODE_ENV=production MONGO_URL=mongodb://localhost:27017/rocketchat ROOT_URL=http://your-host-name.com-as-accessed-from-internet:3000/ PORT=3000\n[Install]\nWantedBy=multi-user.target" | sudo tee /usr/lib/systemd/system/rocketchat.service
+                rm /lib/systemd/system/rocketchat.service
+        read -p "Enter Your Domain (www.example.com) please include www if your using it: "  domain	
+    echo 'MONGO_URL=mongodb://localhost:27017/rocketchat
+ROOT_URL=http://$domain:3000
+PORT=3000' >> /lib/systemd/system/rocketchat.service
+        sudo systemctl enable mongod && sudo systemctl start mongod
+        sudo systemctl enable rocketchat && sudo systemctl start rocketchat
+        sudo firewall-cmd --permanent --add-port=3000/tcp
+        sudo systemctl reload firewalld
     fi    
