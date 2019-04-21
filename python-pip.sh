f699304a14516dffeabd113d12f2750b95d48816
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-04-20 23:29:44 -0400

    Create python-pip.sh

---
+++ python-pip.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [[ "$EUID" -ne 0 ]]; then
+    echo "Sorry, you need to run this as root"
+    exit
+fi
+
+if [ -e /etc/centos-release ]; then
+    DISTRO="CentOS"
+elif [ -e /etc/debian_version ]; then
+    DISTRO=$( lsb_release -is )
+else
+    echo "Your distribution is not supported (yet)"
+    exit
+fi
+
+if [ "$( systemd-detect-virt )" == "openvz" ]; then
+    echo "OpenVZ virtualization is not supported"
+    exit
+fi
+
+    if [ "$DISTRO" == "Ubuntu" ]; then
+        apt-get update
+        apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+        apt-get autoremove -y
+        apt-get clean -y
+        apt-get install python3 -y
+        apt-get install python3-pip -y
+        
+    elif [ "$DISTRO" == "Debian" ]; then
+        apt-get update
+        apt-get upgrade -y
+        apt-get dist-upgrade -y
+        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+        apt-get autoremove -y
+        apt-get clean -y
+        apt-get install python3 -y
+        apt-get install python3-pip -y
+        
+    elif [ "$DISTRO" == "CentOS" ]; then
+        yum update -y
+        yum install epel-release haveged kernel-devel curl -y
+        yum groupinstall 'Development Tools' -y
+        yum install python3 python3-pip -y
+    fi
