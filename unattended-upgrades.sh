6210ab6542439fbaefa430f1759fb7b591c49331
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-08 02:33:05 -0400

    Update unattended-upgrades.sh

--- unattended-upgrades.sh
+++ unattended-upgrades.sh
@@ -16,22 +16,12 @@ fi
 
     if [ "$DISTRO" == "Ubuntu" ]; then
         apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) -y
         apt-get install unattended-upgrades apt-listchanges -y
-        apt-get autoremove -y
-        apt-get clean -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/ubuntu/50unattended-upgrades.Ubuntu"
 
     elif [ "$DISTRO" == "Debian" ]; then
         apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) -y
         apt-get install unattended-upgrades apt-listchanges -y
-        apt-get autoremove -y
-        apt-get clean -y
         wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/debian/50unattended-upgrades.Debian"
         
     elif [ "$DISTRO" == "CentOS" ]; then
