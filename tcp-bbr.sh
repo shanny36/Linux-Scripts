1e26225a0b54a007e75783082304f941a81c40f9
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-05-07 20:31:02 -0400

    Update tcp-bbr.sh

--- tcp-bbr.sh
+++ tcp-bbr.sh
@@ -15,13 +15,21 @@ else
 fi
 
     if [ "$DISTRO" == "Ubuntu" ]; then
+        echo "Updating package list..."
         apt-get update > /dev/null
+        echo "Upgrading package list..."
         apt-get upgrade -y > /dev/null
+        echo "Upgrading Dist list..."
         apt-get dist-upgrade -y > /dev/null
+        echo "Build Essential..."
         apt-get install build-essential haveged linux-headers-$(uname -r) -y > /dev/null
+        echo "Update Keneral..."
         apt-get upgrade linux-generic -y > /dev/null
+        echo "Clean..."
         apt-get clean -y > /dev/null
+        echo "Autoremove..."
         apt-get autoremove -y > /dev/null
+        echo "Systemctl..."
         sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf > /dev/null
         sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf > /dev/null
         echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf > /dev/null
@@ -29,18 +37,30 @@ fi
         sysctl -p > /dev/null
         uname -r
         sysctl net.ipv4.tcp_available_congestion_control
+        echo "Answer Should Be: net.ipv4.tcp_available_congestion_control = bbr cubic reno"
         sysctl net.ipv4.tcp_congestion_control
+        echo "Answer Should Be: net.ipv4.tcp_congestion_control = bbr"
         sysctl net.core.default_qdisc
+        echo "Answer Should Be: net.core.default_qdisc = fq"
         lsmod | grep bbr
+        echo "Answer Should Be: tcp_bbr Modugle Started"
         
     elif [ "$DISTRO" == "Debian" ]; then
+        echo "Updating package list..."
         apt-get update > /dev/null
+        echo "Upgrading package list..."
         apt-get upgrade -y > /dev/null
+        echo "Upgrading Dist list..."
         apt-get dist-upgrade -y > /dev/null
+        echo "Build Essential..."
         apt-get install build-essential haveged linux-headers-$(uname -r) -y > /dev/null
+        echo "Update Keneral..."
         apt-get upgrade linux-base -y > /dev/null
+        echo "Clean..."
         apt-get clean -y > /dev/null
+        echo "Autoremove..."
         apt-get autoremove -y > /dev/null
+        echo "Systemctl..."
         sed -i '/net.core.default_qdisc/d' /etc/sysctl.conf > /dev/null
         sed -i '/net.ipv4.tcp_congestion_control/d' /etc/sysctl.conf > /dev/null
         echo "net.core.default_qdisc = fq" >> /etc/sysctl.conf > /dev/null
@@ -48,9 +68,13 @@ fi
         sysctl -p > /dev/null
         uname -r
         sysctl net.ipv4.tcp_available_congestion_control
+        echo "Answer Should Be: net.ipv4.tcp_available_congestion_control = bbr cubic reno"
         sysctl net.ipv4.tcp_congestion_control
+        echo "Answer Should Be: net.ipv4.tcp_congestion_control = bbr"
         sysctl net.core.default_qdisc
+        echo "Answer Should Be: net.core.default_qdisc = fq"
         lsmod | grep bbr
+        echo "Answer Should Be: tcp_bbr Modugle Started"
         
     elif [ "$DISTRO" == "CentOS" ]; then
         yum update -y
