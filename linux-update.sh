a202f31c8e18c27bcbaa242b9170f99218041ea2
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-06-07 16:28:53 -0400

    Update linux-update.sh

--- linux-update.sh
+++ linux-update.sh
@@ -8,32 +8,19 @@ fi
 if [ -e /etc/centos-release ]; then
     DISTRO="CentOS"
 elif [ -e /etc/debian_version ]; then
-    DISTRO=$( lsb_release -is )
+    DISTRO="Debian"
 elif [ -e /etc/arch-release ]; then
     DISTRO="Arch"
 elif [ -e /etc/fedora-release ]; then
     DISTRO="Fedora"
 elif [ -e /etc/redhat-release ]; then
     DISTRO="Redhat"
+else
 echo "Your distribution is not supported (yet)."
     exit
 fi
 
-    if [ "$DISTRO" == "Ubuntu" ]; then
-        echo "Updating package list..."
-        apt-get update > /dev/null
-        echo "Upgrading package list..."
-        apt-get upgrade -y > /dev/null
-        echo "Upgrading Dist list..."
-        apt-get dist-upgrade -y > /dev/null
-        echo "Build Essential..."
-        apt-get install build-essential linux-headers-$(uname -r) haveged -y > /dev/null
-        echo "Clean..."
-        apt-get clean -y > /dev/null
-        echo "Autoremove..."
-        apt-get autoremove -y > /dev/null
-        
-    elif [ "$DISTRO" == "Debian" ]; then
+    if [ "$DISTRO" == "Debian" ]; then
         echo "Updating package list..."
         apt-get update > /dev/null
         echo "Upgrading package list..."
