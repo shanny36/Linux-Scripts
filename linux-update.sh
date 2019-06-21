b75b4d6f2c4ad71d27ce11bb5cab62e706b055c3
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-06-21 18:37:27 -0400

    Update linux-update.sh

--- linux-update.sh
+++ linux-update.sh
@@ -1,41 +1,69 @@
 #!/bin/bash
 
+## Sanity Checks and automagic
+function root-check() {
 if [[ "$EUID" -ne 0 ]]; then
-    echo "Sorry, you need to run this as root"
-    exit
+  echo "Sorry, you need to run this as root"
+  exit
 fi
+}
 
-if [ -e /etc/centos-release ]; then
+## Root Check
+root-check
+
+## Detect OS
+function dist-check() {
+  if [ -e /etc/centos-release ]; then
     DISTRO="CentOS"
-elif [ -e /etc/debian_version ]; then
-    DISTRO="Debian"
-elif [ -e /etc/arch-release ]; then
+  elif [ -e /etc/debian_version ]; then
+    DISTRO=$( lsb_release -is )
+  elif [ -e /etc/arch-release ]; then
     DISTRO="Arch"
-elif [ -e /etc/fedora-release ]; then
     DISTRO="Fedora"
-elif [ -e /etc/redhat-release ]; then
+  elif [ -e /etc/redhat-release ]; then
     DISTRO="Redhat"
-else
-echo "Your distribution is not supported (yet)."
+  else
+    echo "Your distribution is not supported (yet)."
     exit
-fi
+  fi
+}
 
+## Check distro
+dist-check
+
+## Start Installation
+function install-updates
     if [ "$DISTRO" == "Debian" ]; then
-        echo "Updating package list..."
-        apt-get update > /dev/null
-        echo "Upgrading package list..."
-        apt-get upgrade -y > /dev/null
-        echo "Upgrading Dist list..."
-        apt-get dist-upgrade -y > /dev/null
-        echo "Build Essential..."
-        apt-get install build-essential linux-headers-$(uname -r) haveged -y > /dev/null
-        echo "Clean..."
-        apt-get clean -y > /dev/null
-        echo "Autoremove..."
-        apt-get autoremove -y > /dev/null
-        
+      apt-get update
+      apt-get upgrade -y
+      apt-get dist-upgrade -y
+      apt-get install build-essential linux-headers-$(uname -r) haveged unattended-upgrades apt-listchanges -y
+      wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/config/50unattended-upgrades"
+      apt-get clean -y
+      apt-get autoremove -y
+    elif [ "$DISTRO" == "Ubuntu" ]; then
+      apt-get update
+      apt-get upgrade -y
+      apt-get dist-upgrade -y
+      apt-get install build-essential linux-headers-$(uname -r) haveged unattended-upgrades apt-listchanges -y
+      wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/config/50unattended-upgrades"
+      apt-get clean -y
+      apt-get autoremove -y
+    elif [ "$DISTRO" == "Raspbian" ]; then
+      apt-get update
+      apt-get upgrade -y
+      apt-get dist-upgrade -y
+      apt-get install build-essential linux-headers-$(uname -r) haveged unattended-upgrades apt-listchanges -y
+      wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/config/50unattended-upgrades"
+      apt-get clean -y
+      apt-get autoremove -y
     elif [ "$DISTRO" == "CentOS" ]; then
-        yum update -y
-        yum install epel-release haveged kernel-devel -y
-        yum groupinstall 'Development Tools' -y
+      yum update -y
+      yum install epel-release haveged kernel-devel -y
+      yum groupinstall 'Development Tools' -y
+    elif [ "$DISTRO" == "Fedora" ]; then
+      dnf update
+    elif [ "$DISTRO" == "Redhat" ]; then
+      dnf update -y
     fi
+}
