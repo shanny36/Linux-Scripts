93f92dfe15d672129bef73ac730e7f01e8d1d4e7
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-06-28 17:50:20 -0400

    Create add-ssh-key.sh

---
+++ add-ssh-key.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+## Sanity Checks and automagic
+function root-check() {
+if [[ "$EUID" -ne 0 ]]; then
+  echo "Sorry, you need to run this as root"
+  exit
+fi
+}
+
+## Root Check
+root-check
+
+## Detect OS
+function dist-check() {
+  if [ -e /etc/centos-release ]; then
+    DISTRO="CentOS"
+  elif [ -e /etc/debian_version ]; then
+    DISTRO=$( lsb_release -is )
+  elif [ -e /etc/arch-release ]; then
+    DISTRO="Arch"
+    DISTRO="Fedora"
+  elif [ -e /etc/redhat-release ]; then
+    DISTRO="Redhat"
+  else
+    echo "Your distribution is not supported (yet)."
+    exit
+  fi
+}
+
+## Check distro
+dist-check
+
+## Start Installation Of SSH
+function install-ssh() {
+if [ "$DISTRO" == "Debian" ]; then
+    cd ~
+    pwd
+    mkdir .ssh
+    cd .ssh
+    echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJouQKvkIhLoCyE1lPheITbyIB6ZyEOmAY6e5jEhX6B prajwalkoirala23@protonmail.com' >> ~/.ssh/authorized_keys
+    chmod 700 ~/.ssh/
+    chmod 600 ~/.ssh/authorized_keys
+    echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+    echo 'Port 22' >> /etc/ssh/sshd_config
+    sudo /etc/init.d/ssh restart
+elif [ "$DISTRO" == "Ubuntu" ]; then
+    cd ~
+    pwd
+    mkdir .ssh
+    cd .ssh
+    echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJouQKvkIhLoCyE1lPheITbyIB6ZyEOmAY6e5jEhX6B prajwalkoirala23@protonmail.com' >> ~/.ssh/authorized_keys
+    chmod 700 ~/.ssh/
+    chmod 600 ~/.ssh/authorized_keys
+    echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+    echo 'Port 22' >> /etc/ssh/sshd_config
+    sudo /etc/init.d/ssh restart
+elif [ "$DISTRO" == "Raspbian" ]; then
+    cd ~
+    pwd
+    mkdir .ssh
+    cd .ssh
+    echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJouQKvkIhLoCyE1lPheITbyIB6ZyEOmAY6e5jEhX6B prajwalkoirala23@protonmail.com' >> ~/.ssh/authorized_keys
+    chmod 700 ~/.ssh/
+    chmod 600 ~/.ssh/authorized_keys
+    echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+    echo 'Port 22' >> /etc/ssh/sshd_config
+    sudo /etc/init.d/ssh restart
+elif [ "$DISTRO" == "CentOS" ]; then
+    cd ~
+    pwd
+    mkdir .ssh
+    cd .ssh
+    echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJouQKvkIhLoCyE1lPheITbyIB6ZyEOmAY6e5jEhX6B prajwalkoirala23@protonmail.com' >> ~/.ssh/authorized_keys
+    chmod 700 ~/.ssh/
+    chmod 600 ~/.ssh/authorized_keys
+    echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+    echo 'Port 22' >> /etc/ssh/sshd_config
+    sudo /etc/init.d/ssh restart
+elif [ "$DISTRO" == "Fedora" ]; then
+    cd ~
+    pwd
+    mkdir .ssh
+    cd .ssh
+    echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJouQKvkIhLoCyE1lPheITbyIB6ZyEOmAY6e5jEhX6B prajwalkoirala23@protonmail.com' >> ~/.ssh/authorized_keys
+    chmod 700 ~/.ssh/
+    chmod 600 ~/.ssh/authorized_keys
+    echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+    echo 'Port 22' >> /etc/ssh/sshd_config
+    sudo /etc/init.d/ssh restart
+elif [ "$DISTRO" == "Redhat" ]; then
+    cd ~
+    pwd
+    mkdir .ssh
+    cd .ssh
+    echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEJouQKvkIhLoCyE1lPheITbyIB6ZyEOmAY6e5jEhX6B prajwalkoirala23@protonmail.com' >> ~/.ssh/authorized_keys
+    chmod 700 ~/.ssh/
+    chmod 600 ~/.ssh/authorized_keys
+    echo 'PasswordAuthentication no' >> /etc/ssh/sshd_config
+    echo 'Port 22' >> /etc/ssh/sshd_config
+    sudo /etc/init.d/ssh restart
+fi
+}
+
+## Installation Of SSH
+install-ssh
