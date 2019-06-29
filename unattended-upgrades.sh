f09cc1fe0255e35638b902b45e4a68d71848fe4f
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-06-29 02:40:46 -0400

    Update unattended-upgrades.sh

--- unattended-upgrades.sh
+++ unattended-upgrades.sh
@@ -1,31 +1,59 @@
 #!/bin/bash
 
+## Sanity Checks and automagic
+function root-check() {
 if [[ "$EUID" -ne 0 ]]; then
-    echo "Sorry, you need to run this as root"
-    exit
+  echo "Sorry, you need to run this as root"
+  exit
 fi
+}
 
-if [ -e /etc/centos-release ]; then
+## Root Check
+root-check
+
+## Detect OS
+function dist-check() {
+  if [ -e /etc/centos-release ]; then
     DISTRO="CentOS"
-elif [ -e /etc/debian_version ]; then
+  elif [ -e /etc/debian_version ]; then
     DISTRO=$( lsb_release -is )
-else
-    echo "Your distribution is not supported (yet)"
+  else
+    echo "Your distribution is not supported (yet)."
     exit
-fi
+  fi
+}
 
-    if [ "$DISTRO" == "Ubuntu" ]; then
-        apt-get update
-        apt-get install unattended-upgrades apt-listchanges -y
-        wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/ubuntu/50unattended-upgrades.Ubuntu"
+## Check distro
+dist-check
 
-    elif [ "$DISTRO" == "Debian" ]; then
-        apt-get update
-        apt-get install unattended-upgrades apt-listchanges -y
-        wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/debian/50unattended-upgrades.Debian"
-        
+function install-unattended() {
+  if [ "$DISTRO" == "Ubuntu" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get install unattended-upgrades apt-listchanges -y
+    apt-get autoremove -y
+    apt-get clean -y
+    wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/config/50unattended-upgrades"
+  elif [ "$DISTRO" == "Debian" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get install unattended-upgrades apt-listchanges -y
+    apt-get autoremove -y
+    apt-get clean -y
+    wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/config/50unattended-upgrades"
+  elif [ "$DISTRO" == "Raspbian" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get install unattended-upgrades apt-listchanges -y
+    apt-get autoremove -y
+    apt-get clean -y
+    wget -q -O /etc/apt/apt.conf.d/50unattended-upgrades "https://raw.githubusercontent.com/LiveChief/unattended-upgrades/master/config/50unattended-upgrades"
     elif [ "$DISTRO" == "CentOS" ]; then
-        yum update -y
-        yum install epel-release haveged kernel-devel -y
-        yum groupinstall 'Development Tools' -y
-    fi
+    yum update -y
+    yum install epel-release haveged kernel-devel -y
+    yum groupinstall 'Development Tools' -y
+  fi
+}
+
+## Unattended System Update
+install-unattended
