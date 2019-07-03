00e50184b739d33001a0491ac0f8025f07fe8a40
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-07-02 20:58:54 -0400

    Update chrome-remote-desktop.sh

--- chrome-remote-desktop.sh
+++ chrome-remote-desktop.sh
@@ -31,41 +31,6 @@ function dist-check() {
 ## Check distro
 dist-check
 
-## Start Installation Of SSH
-function install_remote_desktop_compute_engine() {
-#!/bin/bash
-
-## Sanity Checks and automagic
-function root-check() {
-  if [[ "$EUID" -ne 0 ]]; then
-    echo "Sorry, you need to run this as root"
-    exit
-  fi
-}
-
-## Root Check
-root-check
-
-## Detect OS
-function dist-check() {
-  if [ -e /etc/centos-release ]; then
-    DISTRO="CentOS"
-  elif [ -e /etc/debian_version ]; then
-    DISTRO=$(lsb_release -is)
-  elif [ -e /etc/arch-release ]; then
-    DISTRO="Arch"
-    DISTRO="Fedora"
-  elif [ -e /etc/redhat-release ]; then
-    DISTRO="Redhat"
-  else
-    echo "Your distribution is not supported (yet)."
-    exit
-  fi
-}
-
-## Check distro
-dist-check
-
 function remote-desktop-compute() {
   if [ "$DISTRO" == "Debian" ]; then
   wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
