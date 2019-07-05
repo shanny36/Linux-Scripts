87dac554b9acba1062e0417984149514af4f5fa4
Prajwal Koirala <16564273+LiveChief@users.noreply.github.com>
2019-07-04 21:30:38 -0400

    Update Install-Pi-Hole.sh

--- Install-Pi-Hole.sh
+++ Install-Pi-Hole.sh
@@ -34,60 +34,60 @@ dist-check
 
 ## Start Installation
 function install-pihole() {
-    if [ "$DISTRO" == "Debian" ]; then
-        apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
-        apt-get autoremove -y
-        apt-get clean -y
-        curl -sSL https://install.pi-hole.net | bash
-    elif [ "$DISTRO" == "Ubuntu" ]; then
-        apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
-        apt-get autoremove -y
-        apt-get clean -y
-        curl -sSL https://install.pi-hole.net | bash
-    elif [ "$DISTRO" == "Rasbian" ]; then
-        apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
-        apt-get autoremove -y
-        apt-get clean -y
-        curl -sSL https://install.pi-hole.net | bash
-    elif [ "$DISTRO" == "Arch" ]; then
-        apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
-        apt-get autoremove -y
-        apt-get clean -y
-        curl -sSL https://install.pi-hole.net | bash
-    elif [ "$DISTRO" == "Fedora" ]; then
-        apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
-        apt-get autoremove -y
-        apt-get clean -y
-        curl -sSL https://install.pi-hole.net | bash
-    elif [ "$DISTRO" == "Redhat" ]; then
-        apt-get update
-        apt-get upgrade -y
-        apt-get dist-upgrade -y
-        apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
-        apt-get autoremove -y
-        apt-get clean -y
-        curl -sSL https://install.pi-hole.net | bash
-    elif [ "$DISTRO" == "CentOS" ]; then
-        yum update -y
-        yum install epel-release haveged kernel-devel curl -y
-        yum groupinstall 'Development Tools' -y
-        curl -sSL https://install.pi-hole.net | bash
-    fi
+if [ "$DISTRO" == "Debian" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+    apt-get autoremove -y
+    apt-get clean -y
+    curl -sSL https://install.pi-hole.net | bash
+elif [ "$DISTRO" == "Ubuntu" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+    apt-get autoremove -y
+    apt-get clean -y
+    curl -sSL https://install.pi-hole.net | bash
+elif [ "$DISTRO" == "Rasbian" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+    apt-get autoremove -y
+    apt-get clean -y
+    curl -sSL https://install.pi-hole.net | bash
+elif [ "$DISTRO" == "Arch" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+    apt-get autoremove -y
+    apt-get clean -y
+    curl -sSL https://install.pi-hole.net | bash
+elif [ "$DISTRO" == "Fedora" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+    apt-get autoremove -y
+    apt-get clean -y
+    curl -sSL https://install.pi-hole.net | bash
+elif [ "$DISTRO" == "Redhat" ]; then
+    apt-get update
+    apt-get upgrade -y
+    apt-get dist-upgrade -y
+    apt-get install build-essential haveged linux-headers-$(uname -r) curl -y
+    apt-get autoremove -y
+    apt-get clean -y
+    curl -sSL https://install.pi-hole.net | bash
+elif [ "$DISTRO" == "CentOS" ]; then
+    yum update -y
+    yum install epel-release haveged kernel-devel curl -y
+    yum groupinstall 'Development Tools' -y
+    curl -sSL https://install.pi-hole.net | bash
+fi
 }
 ## Install Pi-Hole
 install-pihole
