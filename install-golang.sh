73067dcafec9fed82deda4fea63c5b501ecfc000
Prajwal Koirala <16564273+prajwalkoirala23@users.noreply.github.com>
2019-07-12 21:35:41 -0400

    Update install-golang.sh

--- install-golang.sh
+++ install-golang.sh
@@ -2,30 +2,30 @@
 
 ## Sanity Checks and automagic
 function root-check() {
-if [[ "$EUID" -ne 0 ]]; then
-  echo "Sorry, you need to run this as root"
-  exit
-fi
+    if [[ "$EUID" -ne 0 ]]; then
+        echo "Sorry, you need to run this as root"
+        exit
+    fi
 }
 
 ## Root Check
 root-check
 
 function dist-check() {
-  if [ -e /etc/centos-release ]; then
-    DISTRO="CentOS"
-  elif [ -e /etc/debian_version ]; then
-    DISTRO=$( lsb_release -is )
-  elif [ -e /etc/arch-release ]; then
-    DISTRO="Arch"
-  elif [ -e /etc/fedora-release ]; then
-    DISTRO="Fedora"
-  elif [ -e /etc/redhat-release ]; then
-    DISTRO="Redhat"
-  else
-    echo "Your distribution is not supported (yet)."
-    exit
-  fi
+    if [ -e /etc/centos-release ]; then
+        DISTRO="CentOS"
+    elif [ -e /etc/debian_version ]; then
+        DISTRO=$(lsb_release -is)
+    elif [ -e /etc/arch-release ]; then
+        DISTRO="Arch"
+    elif [ -e /etc/fedora-release ]; then
+        DISTRO="Fedora"
+    elif [ -e /etc/redhat-release ]; then
+        DISTRO="Redhat"
+    else
+        echo "Your distribution is not supported (yet)."
+        exit
+    fi
 }
 
 ## Check distro
@@ -36,16 +36,70 @@ FILE_NAME=$(go1.12.7.linux-$CHECK_ARCHITECTURE.tar.gz)
 
 ## Start Installation
 function install-golang() {
-if [ "$DISTRO" == "Raspbian" ]; then
-cd $HOME
-wget https://dl.google.com/go/$FILE_NAME
-sudo tar -C /usr/local -xvf $FILE_NAME
-cat >> ~/.bashrc << 'EOF'
+    if [ "$DISTRO" == "Raspbian" ]; then
+        cd $HOME
+        wget https://dl.google.com/go/$FILE_NAME
+        sudo tar -C /usr/local -xvf $FILE_NAME
+        cat >>~/.bashrc <<'EOF'
 export GOPATH=$HOME/go
 export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
 EOF
-source ~/.bashrc
-fi
+        source ~/.bashrc
+    elif [ "$DISTRO" == "Debian" ]; then
+        cd $HOME
+        wget https://dl.google.com/go/$FILE_NAME
+        sudo tar -C /usr/local -xvf $FILE_NAME
+        cat >>~/.bashrc <<'EOF'
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+EOF
+        source ~/.bashrc
+    elif [ "$DISTRO" == "Ubuntu" ]; then
+        cd $HOME
+        wget https://dl.google.com/go/$FILE_NAME
+        sudo tar -C /usr/local -xvf $FILE_NAME
+        cat >>~/.bashrc <<'EOF'
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+EOF
+        source ~/.bashrc
+    elif [ "$DISTRO" == "CentOS" ]; then
+        cd $HOME
+        wget https://dl.google.com/go/$FILE_NAME
+        sudo tar -C /usr/local -xvf $FILE_NAME
+        cat >>~/.bashrc <<'EOF'
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+EOF
+        source ~/.bashrc
+    elif [ "$DISTRO" == "Arch" ]; then
+        cd $HOME
+        wget https://dl.google.com/go/$FILE_NAME
+        sudo tar -C /usr/local -xvf $FILE_NAME
+        cat >>~/.bashrc <<'EOF'
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+EOF
+        source ~/.bashrc
+    elif [ "$DISTRO" == "Fedora" ]; then
+        cd $HOME
+        wget https://dl.google.com/go/$FILE_NAME
+        sudo tar -C /usr/local -xvf $FILE_NAME
+        cat >>~/.bashrc <<'EOF'
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+EOF
+        source ~/.bashrc
+    elif [ "$DISTRO" == "Redhat" ]; then
+        cd $HOME
+        wget https://dl.google.com/go/$FILE_NAME
+        sudo tar -C /usr/local -xvf $FILE_NAME
+        cat >>~/.bashrc <<'EOF'
+export GOPATH=$HOME/go
+export PATH=/usr/local/go/bin:$PATH:$GOPATH/bin
+EOF
+        source ~/.bashrc
+    fi
 }
 
 ## Install
